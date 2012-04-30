5d5b678438cf21abb258929421c74812598ff85e
Aldrin D'Souza <a1drin@me.com>
2012-04-30 15:53:27 +0530

    minor changes to css and js

--- web/ajdweb/static/js/ajdweb.js
+++ web/ajdweb/static/js/ajdweb.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
-    
-    $(".page").height($(document).height()/4);
-
-    $(".page").css('margin-bottom', $(document).height()/25);
-
-    $(".page").hover(
-        function(){
-            $(this).addClass("hover");
-        },
-        function(){
-            $(this).removeClass("hover");
-        }
-    );
-
-    $(".page").click(function(){
-        window.location=$(this).find("a").attr("href");
-        return false;
+    $(".page").each(function(){
+        $(this).hover(
+            function() { $(this).addClass("hover"); },
+            function() { $(this).removeClass("hover"); }
+        );
+        $(this).click(function(){
+            window.location=$(this).find("a").attr("href");
+            return false;
+        });
     });
 });
