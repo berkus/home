9379515f5afc156cbd773fe81c9018523de98718
Aldrin D'Souza <a1drin@me.com>
2013-03-06 23:15:58 +0530

    read tree from parenthesised string descriptions.

---
+++ code/c++/misc/tree.cpp
@@ -0,0 +1,112 @@
+/// Copyright (c) 2013, Aldrin's Notebook.
+#include "tree.h"
+
+#include <sstream>
+#include <cassert>
+#include <iostream>
+
+namespace ajd
+{
+  typedef binary_tree<char>::node tree;
+
+  void postorder(tree root, std::ostream &out)
+  {
+    if (root)
+    {
+      postorder(root->left, out);
+      postorder(root->right, out);
+      out << root->value;
+    }
+  }
+
+  void preorder(tree root, std::ostream &out)
+  {
+    if (root)
+    {
+      out << root->value;
+      preorder(root->left, out);
+      preorder(root->right, out);
+    }
+  }
+
+  void inorder(tree root, std::ostream &out)
+  {
+    if (root)
+    {
+      inorder(root->left, out);
+      out << root->value;
+      inorder(root->right, out);
+    }
+  }
+
+  tree from_preorder_inorder(const std::string &preorder, const std::string &inorder)
+  {
+    assert(preorder.size() == inorder.size());
+
+    if (preorder.empty()) { return tree(); }
+
+    std::string::size_type r = inorder.find(preorder[0]);
+
+    std::string left_inorder(inorder, 0, r);
+    std::string left_preorder(preorder, 1, r);
+
+    std::string right_inorder(inorder, r + 1);
+    std::string right_preorder(preorder, r + 1);
+
+    tree root = binary_tree<char>::make_node(preorder[0]);
+    root->left = from_preorder_inorder(left_preorder, left_inorder);
+    root->right = from_preorder_inorder(right_preorder, right_inorder);
+
+    return root;
+  }
+
+  void test_traversals()
+  {
+    tree t(from_preorder_inorder("FBADCEGIH", "ABCDEFGHI"));
+    std::stringstream stream;
+    postorder(t, stream);
+    assert(stream.str() == "ACEDBHIGF");
+  }
+
+  void test_reads()
+  {
+    typedef binary_tree<char> char_tree;
+    typedef char_tree::node ctree;
+    assert(!char_tree::read_tree("()"));
+    ctree one = char_tree::read_tree("(A)");
+    assert(one && one->value == 'A');
+    ctree two = char_tree::read_tree("(A()(C))");
+    assert(!two->left);
+    assert(two->value == 'A');
+    assert(two->right->value == 'C');
+    ctree three = char_tree::read_tree("(A(B)(C))");
+    assert(three->value == 'A');
+    assert(three->left->value == 'B');
+    assert(three->right->value == 'C');
+    ctree four = char_tree::read_tree("(A (B () (D)) (C (E) ()) )");
+    assert(four->value == 'A');
+    assert(four->left->value == 'B');
+    assert(!four->left->left);
+    assert(four->left->right->value == 'D');
+    assert(four->right->value == 'C');
+    assert(four->right->left->value == 'E');
+    assert(!four->right->right);
+
+
+    typedef binary_tree<int> int_tree;
+    typedef int_tree::node itree;
+    itree five = int_tree::read_tree("(0 (-1) (1000 () (123)))");
+    assert(five->value==0);
+    assert(five->left->value==-1);
+    assert(five->right->value==1000);
+    assert(!five->right->left);
+    assert(five->right->right->value==123);
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+  ajd::test_traversals();
+  ajd::test_reads();
+}
--- code/c++/misc/tree.h
+++ code/c++/misc/tree.h
@@ -1,74 +1,72 @@
 /// Copyright (c) 2013, Aldrin's Notebook.
 #pragma once
 
+#include <stack>
 #include <memory>
-#include <cassert>
-#include <ostream>
+#include <sstream>
 
 namespace ajd
 {
-  struct binary_node_t
+  /// A simple binary tree structure to try out algorithm puzzles.
+  template<typename T> struct binary_tree
   {
-    char value;
-    binary_node_t(char v): value(v) {}
-    std::shared_ptr<binary_node_t> left;
-    std::shared_ptr<binary_node_t> right;
+    typedef std::shared_ptr<binary_tree> node;
+
+    T value;
+    node left;
+    node right;
+
+    /// A static factory method for a node.
+    static node make_node(T v) { return std::shared_ptr<binary_tree>(new binary_tree(v)); }
+
+    /// A static builder method for a tree from a string input. The input is expected to be a fully
+    /// parenthesized expressions of the form:
+
+    /// tree  := '(' value tree tree ')' | '()'
+    /// value := any string that can be converted to T
+
+    /// Examples: (), (A () (B)), (-1 (0 (1000 (2) (123)) ()) ())
+
+    /// This code is for solving algorithm-puzzles. It does the right thing when given the right
+    /// input. There is limited input validation and no error reporting, please mind your inputs.
+    static node read_tree(std::string input)
+    {
+      // trim.
+      input.erase(input.find_last_not_of(" )") + 1);
+      input.erase(0, input.find_first_not_of(" ("));
+
+      // if no input, return null node.
+      if (input.empty()) { return node(); }
+
+      // everything upto the first parenthesis is the value of the root node.
+      size_t i(0);
+      std::stringstream stream;
+      while (i < input.length() && input.at(i) != '(') { stream << input.at(i++); }
+      T rootval = T();
+      stream >> rootval;
+      node root = make_node(rootval);
+
+      // if we have input left, we make left and right subtrees.
+      if (i < input.length())
+      {
+        size_t left_start(i);
+        std::stack<char> stack;
+        stack.push(input.at(i)); // input[i] has to be '('
+        while (i < input.length() && !stack.empty()) // loop till we close the first '('
+        {
+          char next = input.at(++i);
+          if (next == '(') {stack.push(next);}
+          if (next == ')') {stack.pop(); }
+        }
+        size_t left_end = i;
+        root->left = read_tree(input.substr(left_start, left_end - left_start + 1));
+        root->right = read_tree(input.substr(left_end + 1));
+      }
+
+      return root;
+    }
+
+  private:
+    binary_tree(T v): value(v) {}
   };
-
-  typedef std::shared_ptr<binary_node_t> binary_tree;
-
-  bool is_leaf(binary_tree n) { return !(n->left || n->right); }
-
-  binary_tree make_binary_node(char c) { return std::make_shared<binary_node_t>(c); }
-
-  binary_tree read_binary_tree(const std::string &preorder, const std::string &inorder)
-  {
-    assert(preorder.size() == inorder.size());
-
-    if (preorder.empty()) { return binary_tree(); }
-
-    std::string::size_type r = inorder.find(preorder[0]);
-
-    std::string left_inorder(inorder, 0, r);
-    std::string right_inorder(inorder, r + 1);
-    std::string left_preorder(preorder, 1, r);
-    std::string right_preorder(preorder, r + 1);
-
-    binary_tree root = make_binary_node(preorder[0]);
-    root->left = read_binary_tree(left_preorder, left_inorder);
-    root->right = read_binary_tree(right_preorder, right_inorder);
-
-    return root;
-  }
-
-  void postorder(binary_tree root, std::ostream &out)
-  {
-    if (root)
-    {
-      postorder(root->left, out);
-      postorder(root->right, out);
-      out << root->value;
-    }
-  }
-
-  void preorder(binary_tree root, std::ostream &out)
-  {
-    if (root)
-    {
-      out << root->value;
-      preorder(root->left, out);
-      preorder(root->right, out);
-    }
-  }
-
-  void inorder(binary_tree root, std::ostream &out)
-  {
-    if (root)
-    {
-      inorder(root->left, out);
-      out << root->value;
-      inorder(root->right, out);
-    }
-  }
-
 }
